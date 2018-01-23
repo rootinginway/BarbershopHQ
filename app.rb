89bcac192310808c7d89e93f4bb5e1011dcc6274
rootinginway <romeopanasak@gmail.com>
2018-01-23 14:55:14 +0200

    Init commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+end
+
+class Barber < ActiveRecord::Base
+end
+
+get '/' do
+	@barbers = Barber.order "created_at DESC"
+	erb :index
+end
